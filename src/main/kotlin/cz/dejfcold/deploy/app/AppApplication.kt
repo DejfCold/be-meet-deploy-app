74ebac576eb7a63ab4df8b255befeb0b9324fcbe
dejfcold <dejfcold@protonmail.ch>
2024-12-09 19:13:31 +0100

    Initial commit

---
+++ src/main/kotlin/cz/dejfcold/deploy/app/AppApplication.kt
@@ -0,0 +1,11 @@
+package cz.dejfcold.deploy.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppApplication
+
+fun main(args: Array<String>) {
+    runApplication<AppApplication>(*args)
+}
